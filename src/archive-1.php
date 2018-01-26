1670fbc62a21dcc0b873c4db0378b316c1eee85e
Unknown <leszek@lubcon.org>
2018-01-26 03:13:32 +0100

    [WIP] Add archive

---
+++ src/archive-1.php
@@ -0,0 +1,59 @@
+<div class="tab-pane fade show active" id="pills-all" role="tabpanel" aria-labelledby="pills-all-tab">
+    <div class="cards d-flex flex-row justify-content-around flex-wrap">
+        <button class="cards__item card text-center mb-5" type="button" data-toggle="modal" data-target=".newModal1">
+                        <!--HOTFIX: modal bootstrap onli works in safari cart isn in button -->
+                            <div>
+                                <p class="cards__item--image card-img-top img-fluid"></p>
+                                <div class="cards__item--text card-block mt-4">
+                                    <h5 class="card-title text-primary font-weight-bold">Name preject</h5>
+                                    <p class="cards__item--text text-14px text-gray-500 card-text font-weight-light">designed by Authour | May 23, 2014 </p>
+                                </div>
+                            </div>
+                        </button>
+        <button class="cards__item card text-center mb-5" type="button" data-toggle="modal" data-target=".newModal1">
+                            <div>
+                                <p class="cards__item--image card-img-top img-fluid"></p>
+                                <div class="cards__item--text card-block mt-4">
+                                    <h5 class="card-title text-primary font-weight-bold">Name preject</h5>
+                                    <p class="cards__item--text text-14px text-gray-500 card-text font-weight-light">designed by Authour | May 23, 2014 </p>
+                                </div>
+                            </div>
+                        </button>
+        <button class="cards__item card text-center mb-5" type="button" data-toggle="modal" data-target=".newModal1">
+                            <div>
+                                <p class="cards__item--image card-img-top img-fluid"></p>
+                                <div class="cards__item--text card-block mt-4">
+                                    <h5 class="card-title text-primary font-weight-bold">Name preject</h5>
+                                    <p class="cards__item--text text-14px text-gray-500 card-text font-weight-light">designed by Authour | May 23, 2014 </p>
+                                </div>
+                            </div>
+                        </button>
+        <button class="cards__item card text-center mb-5" type="button" data-toggle="modal" data-target=".newModal1">
+                            <div>
+                                <p class="cards__item--image card-img-top img-fluid"></p>
+                                <div class="cards__item--text card-block mt-4">
+                                    <h5 class="card-title text-primary font-weight-bold">Name preject</h5>
+                                    <p class="cards__item--text text-14px text-gray-500 card-text font-weight-light">designed by Authour | May 23, 2014 </p>
+                                </div>
+                            </div>
+                        </button>
+        <button class="cards__item card text-center mb-5" type="button" data-toggle="modal" data-target=".newModal1">
+                            <div>
+                                <p class="cards__item--image card-img-top img-fluid"></p>
+                                <div class="cards__item--text card-block mt-4">
+                                    <h5 class="card-title text-primary font-weight-bold">Name preject</h5>
+                                    <p class="cards__item--text text-14px text-gray-500 card-text font-weight-light">designed by Authour | May 23, 2014 </p>
+                                </div>
+                            </div>
+                        </button>
+        <button class="cards__item card text-center mb-5" type="button" data-toggle="modal" data-target=".newModal1">
+                            <div>
+                                <p class="cards__item--image card-img-top img-fluid"></p>
+                                <div class="cards__item--text card-block mt-4">
+                                    <h5 class="card-title text-primary font-weight-bold">Name preject</h5>
+                                    <p class="cards__item--text text-14px text-gray-500 card-text font-weight-light">designed by Authour | May 23, 2014 </p>
+                                </div>
+                            </div>
+                        </button>
+    </div>
+</div>
---
+++ src/archive-2.php
@@ -0,0 +1,58 @@
+<div class="tab-pane fade" id="pills-website" role="tabpanel" aria-labelledby="pills-website-tab">
+                    <div class="cards d-flex flex-row justify-content-around flex-wrap">
+                        <button class="cards__item card text-center mb-5" type="button" data-toggle="modal" data-target=".newModal1">
+                            <div>
+                                <p class="cards__item--image card-img-top img-fluid"></p>
+                                <div class="cards__item--text card-block mt-4">
+                                    <h5 class="card-title text-primary font-weight-bold">Name preject</h5>
+                                    <p class="cards__item--text text-14px text-gray-500 card-text font-weight-light">designed by Authour | May 23, 2014 </p>
+                                </div>
+                            </div>
+                        </button>
+                        <button class="cards__item card text-center mb-5" type="button" data-toggle="modal" data-target=".newModal1">
+                            <div>
+                                <p class="cards__item--image card-img-top img-fluid"></p>
+                                <div class="cards__item--text card-block mt-4">
+                                    <h5 class="card-title text-primary font-weight-bold">Name preject</h5>
+                                    <p class="cards__item--text text-14px text-gray-500 card-text font-weight-light">designed by Authour | May 23, 2014 </p>
+                                </div>
+                            </div>
+                        </button>
+                        <button class="cards__item card text-center mb-5" type="button" data-toggle="modal" data-target=".newModal1">
+                            <div>
+                                <p class="cards__item--image card-img-top img-fluid"></p>
+                                <div class="cards__item--text card-block mt-4">
+                                    <h5 class="card-title text-primary font-weight-bold">Name preject</h5>
+                                    <p class="cards__item--text text-14px text-gray-500 card-text font-weight-light">designed by Authour | May 23, 2014 </p>
+                                </div>
+                            </div>
+                        </button>
+                        <button class="cards__item card text-center mb-5" type="button" data-toggle="modal" data-target=".newModal1">
+                            <div>
+                                <p class="cards__item--image card-img-top img-fluid"></p>
+                                <div class="cards__item--text card-block mt-4">
+                                    <h5 class="card-title text-primary font-weight-bold">Name preject</h5>
+                                    <p class="cards__item--text text-14px text-gray-500 card-text font-weight-light">designed by Authour | May 23, 2014 </p>
+                                </div>
+                            </div>
+                        </button>
+                        <button class="cards__item card text-center mb-5" type="button" data-toggle="modal" data-target=".newModal1">
+                            <div>
+                                <p class="cards__item--image card-img-top img-fluid"></p>
+                                <div class="cards__item--text card-block mt-4">
+                                    <h5 class="card-title text-primary font-weight-bold">Name preject</h5>
+                                    <p class="cards__item--text text-14px text-gray-500 card-text font-weight-light">designed by Authour | May 23, 2014 </p>
+                                </div>
+                            </div>
+                        </button>
+                        <button class="cards__item card text-center mb-5" type="button" data-toggle="modal" data-target=".newModal1">
+                            <div>
+                                <p class="cards__item--image card-img-top img-fluid"></p>
+                                <div class="cards__item--text card-block mt-4">
+                                    <h5 class="card-title text-primary font-weight-bold">Name preject</h5>
+                                    <p class="cards__item--text text-14px text-gray-500 card-text font-weight-light">designed by Authour | May 23, 2014 </p>
+                                </div>
+                            </div>
+                        </button>
+                    </div>
+                </div>
